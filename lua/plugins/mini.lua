de73ac391a180413de0adc2605aeaab1304947f0
Jhonnikek <138178283+Jhonnikek@users.noreply.github.com>
2026-03-27 12:42:56 -0500

    customize statusline

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -7,7 +7,25 @@ return {
 
       local statusline = require 'mini.statusline'
       statusline.setup { use_icons = vim.g.have_nerd_font }
-      statusline.section_location = function() return '%2l:%-2v' end
+      statusline.active = function()
+        local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 120 }
+        local git = MiniStatusline.section_git { trunc_width = 40 }
+        local diff = MiniStatusline.section_diff { trunc_width = 75 }
+        local filename = '%t %m %r'
+        local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 160 }
+        local search = MiniStatusline.section_searchcount { trunc_width = 75 }
+        local location = '%2l:%-2v'
+
+        return MiniStatusline.combine_groups {
+          { hl = mode_hl, strings = { mode } },
+          { hl = 'MiniStatuslineDevinfo', strings = { git, diff } },
+          '%<', -- Mark general truncate point
+          { hl = 'MiniStatuslineFilename', strings = { filename } },
+          '%=', -- End left alignment
+          { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+          { hl = mode_hl, strings = { search, location } },
+        }
+      end
 
       local hipatterns = require 'mini.hipatterns'
       hipatterns.setup {
