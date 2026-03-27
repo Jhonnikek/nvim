b23745adebde2227e09f76ed68def2f78082ac8f
Jhonnikek <138178283+Jhonnikek@users.noreply.github.com>
2026-03-26 23:38:20 -0500

    feat: add bufferline plugin

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    'akinsho/bufferline.nvim',
+    event = 'VeryLazy',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      options = {
+        close_command = function(n) require('snacks').bufdelete(n) end,
+        right_mouse_command = function(n) require('snacks').bufdelete(n) end,
+        always_show_bufferline = false,
+
+        offsets = {
+          {
+            filetype = 'snacks_layout_box',
+            text = 'File Explorer',
+            highlight = 'Directory',
+            separator = true,
+          },
+        },
+      },
+    },
+  },
+}
