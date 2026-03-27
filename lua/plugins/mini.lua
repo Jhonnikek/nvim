d409a10bab6be2ac56002c9f897a5771115c7c4e
Jhonnikek <138178283+Jhonnikek@users.noreply.github.com>
2026-03-26 23:37:01 -0500

    feat: add mini nvim config

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    'nvim-mini/mini.nvim',
+    event = 'VeryLazy',
+    config = function()
+      require('mini.ai').setup { n_lines = 500 }
+
+      local statusline = require 'mini.statusline'
+      statusline.setup { use_icons = vim.g.have_nerd_font }
+      statusline.section_location = function() return '%2l:%-2v' end
+
+      local hipatterns = require 'mini.hipatterns'
+      hipatterns.setup {
+        highlighters = {
+          fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+          hack = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
+          todo = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
+          note = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
+
+          hex_color = hipatterns.gen_highlighter.hex_color(),
+        },
+      }
+    end,
+  },
+}
