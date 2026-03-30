dabbcd6a41fa2246cdeb90f59463b2d0a0b11d6c
Jhonnikek <138178283+Jhonnikek@users.noreply.github.com>
2026-03-30 12:48:00 -0500

    update autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,10 +1,3 @@
-vim.api.nvim_create_autocmd('VimEnter', {
-  callback = function()
-    local arg = vim.fn.argv(0)
-    if arg and arg ~= '' and vim.fn.isdirectory(arg) == 1 then vim.api.nvim_set_current_dir(arg) end
-  end,
-})
-
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
