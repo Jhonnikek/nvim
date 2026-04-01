eeb0f6b929ebd2027164155b57b769706f5a1819
Jhonnikek <138178283+Jhonnikek@users.noreply.github.com>
2026-03-31 22:14:55 -0500

    update noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -4,7 +4,6 @@ return {
     event = 'VeryLazy',
     dependencies = {
       'MunifTanjim/nui.nvim',
-      'folke/snacks.nvim',
     },
     opts = {
       lsp = {
@@ -13,9 +12,7 @@ return {
           ['vim.lsp.util.stylize_markdown'] = true,
           ['cmp.entry.get_documentation'] = true,
         },
-        progress = {
-          enabled = false,
-        },
+        progress = { enabled = false },
       },
       routes = {
         {
